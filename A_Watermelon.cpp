7a3297eb6ec67d645cf95c91c459e50cb02558ee
Aviral <aviralsheoran.2002@gmail.com>
2022-07-24 13:32:11 +0530

    My First problem from atoj ladders

---
+++ A_Watermelon.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    if (t <= 2 || t % 2 != 0)
+    {
+        cout << "NO" << '\n';
+    }
+    else
+    {
+        cout << "YES" << '\n';
+    }
+
+    return 0;
+}
